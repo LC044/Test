0a31dcc5f0ef5895b984e2e29bc2a4df20347093
shuaikangzhou <863909694@qq.com>
2022-10-04 00:58:15 +0800

    Changes

---
+++ new.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @FileName  :new.py
+# @Time      :2022/10/4 上午12:13
+# @Author    :ShuaikangZhou
+
+
+if __name__ == "__main__":
+    run_code = 0
+    print('hello')
